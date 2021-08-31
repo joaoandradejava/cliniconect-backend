7e0a495c8952489ab01552f79bf9096b4d73e224
joaoandradejava <andradepinheironeto24@hotmail.com>
2021-08-31 13:42:48 -0300

    Primeiro commit

---
+++ src/main/java/com/cliniconect/sistema/CliniconectBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cliniconect.sistema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CliniconectBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CliniconectBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cliniconect/sistema/CliniconectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cliniconect.sistema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CliniconectBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
